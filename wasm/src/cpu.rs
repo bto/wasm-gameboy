305baa30080d150b108a81bf6ac61354f09a598d
Masato Bito <m@btmst.jp>
2020-11-15 21:06:38 +0900

    add op_add()

--- wasm/src/cpu.rs
+++ wasm/src/cpu.rs
@@ -7,14 +7,19 @@ struct CPU {
 }
 
 impl CPU {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             bus: MemoryBus::new(),
             registers: Registers::new(),
         }
     }
 
-    fn execute(&self, byte: u8) {
+    pub fn step(&mut self) {
+        let byte = self.bus.get_byte(self.registers.pc);
+        self.execute(byte);
+    }
+
+    fn execute(&mut self, byte: u8) {
         let bits = (
             byte & 0b00000001,
             (byte & 0b00000010) >> 1,
@@ -28,13 +33,27 @@ impl CPU {
 
         match bits {
             (0, 0, 1, 1, 1, 0, 1, 0) => self.op_lda(),
+            (1, 0, 0, 0, 0, _, _, _) => self.op_add(byte),
             _ => panic!("not implemented"),
         }
     }
 
-    fn step(&self) {
-        let byte = self.bus.get_byte(self.registers.pc);
-        self.execute(byte);
+    fn get_register_8(&self, byte: u8) -> u8 {
+        match byte & 0b111 {
+            0b111 => self.registers.a,
+            0b000 => self.registers.b,
+            0b001 => self.registers.c,
+            0b010 => self.registers.d,
+            0b011 => self.registers.e,
+            0b100 => self.registers.h,
+            0b101 => self.registers.l,
+            0b110 => (self.bus.get_byte(self.registers.get_hl() as usize)),
+            _ => panic!("never reach"),
+        }
+    }
+
+    fn op_add(&mut self, byte: u8) {
+        self.registers.a += self.get_register_8(byte);
     }
 
     fn op_lda(&self) {
