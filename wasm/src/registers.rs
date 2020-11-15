af697b5fec5d38ff000acb75a9a648ae10ec55f3
Masato Bito <m@btmst.jp>
2020-11-15 16:40:02 +0900

    move to global definition to lib.rs

--- wasm/src/registers.rs
+++ wasm/src/registers.rs
@@ -1,11 +1,3 @@
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 struct Registers {
     a: u8,
     b: u8,
