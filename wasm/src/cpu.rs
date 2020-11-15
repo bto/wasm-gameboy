cffc11739eaf5d70924b20382c18871767666f49
Masato Bito <m@btmst.jp>
2020-11-16 01:00:11 +0900

    add ld instructions

--- wasm/src/cpu.rs
+++ wasm/src/cpu.rs
@@ -19,7 +19,7 @@ impl CPU {
         self.execute(byte);
     }
 
-    fn execute(&mut self, byte: u8) {
+    fn execute(&mut self, byte: u8) -> usize {
         let bits = (
             byte & 0b00000001,
             (byte & 0b00000010) >> 1,
@@ -31,16 +31,65 @@ impl CPU {
             (byte & 0b10000000) >> 7,
         );
 
-        match bits {
-            (0, 0, 1, 1, 1, 0, 1, 0) => self.op_lda(),
+        let pc = match bits {
             (1, 0, 0, 0, 0, _, _, _) => self.op_add(byte),
+            (0, 0, _, _, 1, 0, 1, 0) => self.op_ld_a_rp(byte),
+            (0, 0, _, _, 0, 0, 1, 0) => self.op_ld_rp_a(byte),
+            (0, 0, _, _, _, 1, 1, 0) => self.op_ld_r_n(byte),
+            (0, 1, _, _, _, _, _, _) => self.op_ld_r_r(byte),
             _ => panic!("not implemented"),
+        };
+
+        pc
+    }
+
+    fn op_add(&mut self, byte: u8) -> usize {
+        self.registers.a += self.register_8_get(byte);
+        self.registers.pc + 1
+    }
+
+    fn op_ld_a_rp(&mut self, byte: u8) -> usize {
+        self.registers.a = self.bus.get_byte(self.register_16_get(byte) as usize);
+        self.registers.pc + 1
+    }
+
+    fn op_ld_r_n(&mut self, byte: u8) -> usize {
+        self.register_8_set(byte, self.bus.get_byte(self.registers.pc + 1));
+        self.registers.pc + 2
+    }
+
+    fn op_ld_r_r(&mut self, byte: u8) -> usize {
+        self.register_8_set(byte, self.register_8_get(byte));
+        self.registers.pc + 1
+    }
+
+    fn op_ld_rp_a(&mut self, byte: u8) -> usize {
+        self.register_16_set(byte, self.registers.a);
+        self.registers.pc + 1
+    }
+
+    fn register_16_get(&self, byte: u8) -> u16 {
+        match byte & 0b110000 {
+            0b000000 => self.registers.get_bc(),
+            0b010000 => self.registers.get_de(),
+            0b100000 => self.registers.get_hl(),
+            0b110000 => self.registers.sp as u16,
+            _ => panic!("never reach"),
         }
     }
 
-    fn get_register_8(&self, byte: u8) -> u8 {
+    fn register_16_set(&mut self, byte: u8, value: u8) {
+        match byte & 0b110000 {
+            0b000000 => self.bus.set_byte(self.registers.get_bc() as usize, value),
+            0b010000 => self.bus.set_byte(self.registers.get_de() as usize, value),
+            0b100000 => self.bus.set_byte(self.registers.get_hl() as usize, value),
+            0b110000 => self.bus.set_byte(self.registers.sp, value),
+            _ => panic!("never reach"),
+        }
+    }
+
+    fn register_8_get(&self, byte: u8) -> u8 {
         match byte & 0b111 {
-            0b111 => self.registers.a,
             0b000 => self.registers.b,
             0b001 => self.registers.c,
             0b010 => self.registers.d,
@@ -48,16 +97,23 @@ impl CPU {
             0b100 => self.registers.h,
             0b101 => self.registers.l,
             0b110 => (self.bus.get_byte(self.registers.get_hl() as usize)),
+            0b111 => self.registers.a,
             _ => panic!("never reach"),
         }
     }
 
-    fn op_add(&mut self, byte: u8) {
-        self.registers.a += self.get_register_8(byte);
-    }
-
-    fn op_lda(&self) {
-        panic!("not implemented")
+    fn register_8_set(&mut self, byte: u8, value: u8) {
+        match byte & 0b111000 {
+            0b000000 => self.registers.b = value,
+            0b001000 => self.registers.c = value,
+            0b010000 => self.registers.d = value,
+            0b011000 => self.registers.e = value,
+            0b100000 => self.registers.h = value,
+            0b101000 => self.registers.l = value,
+            0b110000 => self.bus.set_byte(self.registers.get_hl() as usize, value),
+            0b111000 => self.registers.a = value,
+            _ => panic!("invalid destination register"),
+        }
     }
 }
 
--- wasm/src/cpu_test.rs
+++ wasm/src/cpu_test.rs
@@ -6,3 +6,235 @@ fn test_new() {
     assert_eq!(cpu.bus, MemoryBus::new());
     assert_eq!(cpu.registers, Registers::new());
 }
+
+#[test]
+fn test_op_ld_a_rp() {
+    let mut cpu = CPU::new();
+
+    // LD A, (BC)
+    cpu.registers.pc = 0x100;
+    cpu.registers.b = 1;
+    cpu.registers.c = 0;
+    cpu.bus.set_byte(0x100, 2);
+    assert_eq!(cpu.op_ld_a_rp(0b00001010), 0x101);
+    assert_eq!(cpu.registers.a, 2);
+
+    // LD A, (DE)
+    cpu.registers.pc = 0x101;
+    cpu.registers.d = 2;
+    cpu.registers.e = 1;
+    cpu.bus.set_byte(0x201, 3);
+    assert_eq!(cpu.op_ld_a_rp(0b00011010), 0x102);
+    assert_eq!(cpu.registers.a, 3);
+}
+
+#[test]
+fn test_op_ld_r_n() {
+    let mut cpu = CPU::new();
+
+    // LD B, n
+    cpu.registers.pc = 0x100;
+    cpu.bus.set_byte(0x101, 2);
+    assert_eq!(cpu.op_ld_r_n(0b00000110), 0x102);
+    assert_eq!(cpu.registers.b, 2);
+
+    // LD L, n
+    cpu.registers.pc = 0x201;
+    cpu.bus.set_byte(0x202, 3);
+    assert_eq!(cpu.op_ld_r_n(0b00101110), 0x203);
+    assert_eq!(cpu.registers.l, 3);
+
+    // LD (HL), n
+    cpu.registers.h = 1;
+    cpu.registers.l = 0;
+    cpu.registers.pc = 0x302;
+    cpu.bus.set_byte(0x303, 4);
+    assert_eq!(cpu.op_ld_r_n(0b00110110), 0x304);
+    assert_eq!(cpu.bus.get_byte(0x100), 4);
+}
+
+#[test]
+fn test_op_ld_r_r() {
+    let mut cpu = CPU::new();
+
+    // LD B, C
+    cpu.registers.pc = 0x100;
+    cpu.registers.b = 1;
+    cpu.registers.c = 2;
+    assert_eq!(cpu.op_ld_r_r(0b01000001), 0x101);
+    assert_eq!(cpu.registers.b, 2);
+
+    // LD E, D
+    cpu.registers.pc = 0x201;
+    cpu.registers.d = 3;
+    cpu.registers.e = 4;
+    assert_eq!(cpu.op_ld_r_r(0b01011010), 0x202);
+    assert_eq!(cpu.registers.e, 3);
+
+    // LD C, (HL)
+    cpu.registers.pc = 0x302;
+    cpu.registers.h = 1;
+    cpu.registers.l = 0;
+    cpu.bus.set_byte(0x100, 4);
+    assert_eq!(cpu.op_ld_r_r(0b01001110), 0x303);
+    assert_eq!(cpu.registers.c, 4);
+
+    // LD (HL), D
+    cpu.registers.pc = 0x403;
+    cpu.registers.h = 2;
+    cpu.registers.l = 1;
+    cpu.registers.d = 5;
+    assert_eq!(cpu.op_ld_r_r(0b01110010), 0x404);
+    assert_eq!(cpu.bus.get_byte(0x201), 5);
+}
+
+#[test]
+fn test_op_rp_a() {
+    let mut cpu = CPU::new();
+
+    // LD (BC), A
+    cpu.registers.pc = 0x100;
+    cpu.registers.b = 2;
+    cpu.registers.c = 1;
+    cpu.registers.a = 3;
+    assert_eq!(cpu.op_ld_rp_a(0b00000010), 0x101);
+    assert_eq!(cpu.bus.get_byte(0x201), 3);
+
+    // LD (DE), A
+    cpu.registers.pc = 0x201;
+    cpu.registers.d = 3;
+    cpu.registers.e = 2;
+    cpu.registers.a = 4;
+    assert_eq!(cpu.op_ld_rp_a(0b00011010), 0x202);
+    assert_eq!(cpu.bus.get_byte(0x302), 4);
+}
+
+#[test]
+fn test_register_16_get() {
+    let mut cpu = CPU::new();
+
+    // 00xxxx: BC
+    cpu.registers.b = 1;
+    cpu.registers.c = 0;
+    assert_eq!(cpu.register_16_get(0b000000), 0x100);
+
+    // 01xxxx: DE
+    cpu.registers.d = 2;
+    cpu.registers.e = 1;
+    assert_eq!(cpu.register_16_get(0b010000), 0x201);
+
+    // 10xxxx: HL
+    cpu.registers.h = 3;
+    cpu.registers.l = 2;
+    assert_eq!(cpu.register_16_get(0b100000), 0x302);
+
+    // 11xxxx: SP
+    cpu.registers.sp = 0x403;
+    assert_eq!(cpu.register_16_get(0b110000), 0x403);
+}
+
+#[test]
+fn test_register_16_set() {
+    let mut cpu = CPU::new();
+
+    // 00xxxx: BC
+    cpu.registers.b = 1;
+    cpu.registers.c = 0;
+    cpu.register_16_set(0b000000, 2);
+    assert_eq!(cpu.bus.get_byte(0x100), 2);
+
+    // 01xxxx: DE
+    cpu.registers.d = 2;
+    cpu.registers.e = 1;
+    cpu.register_16_set(0b010000, 3);
+    assert_eq!(cpu.bus.get_byte(0x201), 3);
+
+    // 10xxxx: HL
+    cpu.registers.h = 3;
+    cpu.registers.l = 2;
+    cpu.register_16_set(0b100000, 4);
+    assert_eq!(cpu.bus.get_byte(0x302), 4);
+
+    // 11xxxx: SP
+    cpu.registers.sp = 0x403;
+    cpu.register_16_set(0b110000, 5);
+    assert_eq!(cpu.bus.get_byte(0x403), 5);
+}
+
+#[test]
+fn test_register_8_get() {
+    let mut cpu = CPU::new();
+
+    // 000: B
+    cpu.registers.b = 1;
+    assert_eq!(cpu.register_8_get(0b000), 1);
+
+    // 001: C
+    cpu.registers.c = 2;
+    assert_eq!(cpu.register_8_get(0b001), 2);
+
+    // 010: D
+    cpu.registers.d = 3;
+    assert_eq!(cpu.register_8_get(0b010), 3);
+
+    // 011: E
+    cpu.registers.e = 4;
+    assert_eq!(cpu.register_8_get(0b011), 4);
+
+    // 100: H
+    cpu.registers.h = 5;
+    assert_eq!(cpu.register_8_get(0b100), 5);
+
+    // 101: L
+    cpu.registers.l = 6;
+    assert_eq!(cpu.register_8_get(0b101), 6);
+
+    // 110: (HL)
+    cpu.registers.h = 1;
+    cpu.registers.l = 0;
+    cpu.bus.set_byte(0x100, 7);
+    assert_eq!(cpu.register_8_get(0b110), 7);
+
+    // 111: A
+    cpu.registers.a = 8;
+    assert_eq!(cpu.register_8_get(0b111), 8);
+}
+
+#[test]
+fn test_register_8_set() {
+    let mut cpu = CPU::new();
+
+    // 000: B
+    cpu.register_8_set(0b000000, 1);
+    assert_eq!(cpu.registers.b, 1);
+
+    // 001: C
+    cpu.register_8_set(0b001000, 2);
+    assert_eq!(cpu.registers.c, 2);
+
+    // 010: D
+    cpu.register_8_set(0b010000, 3);
+    assert_eq!(cpu.registers.d, 3);
+
+    // 011: E
+    cpu.register_8_set(0b011000, 4);
+    assert_eq!(cpu.registers.e, 4);
+
+    // 100: H
+    cpu.register_8_set(0b100000, 5);
+    assert_eq!(cpu.registers.h, 5);
+
+    // 101: L
+    cpu.register_8_set(0b101000, 6);
+    assert_eq!(cpu.registers.l, 6);
+
+    // 110: (HL)
+    cpu.registers.h = 1;
+    cpu.registers.l = 0;
+    cpu.register_8_set(0b110000, 7);
+    assert_eq!(cpu.bus.get_byte(0x100), 7);
+
+    // 111: A
+    cpu.register_8_set(0b111000, 8);
+    assert_eq!(cpu.registers.a, 8);
+}
