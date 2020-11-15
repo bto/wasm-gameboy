9a4f75d57d99b19f4be602f82afd56374d0b7336
Masato Bito <m@btmst.jp>
2020-11-15 18:23:44 +0900

    add program counter

--- wasm/src/registers_test.rs
+++ wasm/src/registers_test.rs
@@ -10,6 +10,7 @@ fn test_new() {
     assert_eq!(registers.e, 0);
     assert_eq!(registers.h, 0);
     assert_eq!(registers.l, 0);
+    assert_eq!(registers.pc, 0);
     assert_eq!(registers.carry, false);
     assert_eq!(registers.half_carry, false);
     assert_eq!(registers.subtraction, false);
