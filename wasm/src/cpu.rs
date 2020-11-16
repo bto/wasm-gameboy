4629f71e37d3306c04ec9cb07377a5056328b15e
Masato Bito <m@btmst.jp>
2020-11-16 18:35:35 +0900

    s/byte/opcode/g

--- wasm/src/cpu.rs
+++ wasm/src/cpu.rs
@@ -15,8 +15,8 @@ impl CPU {
     }
 
     pub fn step(&mut self) {
-        let byte = self.bus.byte_get(self.registers.pc);
-        self.execute(byte);
+        let opcode = self.bus.byte_get(self.registers.pc);
+        self.execute(opcode);
     }
 
     fn db_get(&self) -> u8 {
@@ -29,16 +29,16 @@ impl CPU {
         (hb as u16) << 8 | lb as u16
     }
 
-    fn execute(&mut self, byte: u8) -> u16 {
+    fn execute(&mut self, opcode: u8) -> u16 {
         let bits = (
-            (byte & 0b10000000) >> 7,
-            (byte & 0b01000000) >> 6,
-            (byte & 0b00100000) >> 5,
-            (byte & 0b00010000) >> 4,
-            (byte & 0b00001000) >> 3,
-            (byte & 0b00000100) >> 2,
-            (byte & 0b00000010) >> 1,
-            byte & 0b00000001,
+            (opcode & 0b10000000) >> 7,
+            (opcode & 0b01000000) >> 6,
+            (opcode & 0b00100000) >> 5,
+            (opcode & 0b00010000) >> 4,
+            (opcode & 0b00001000) >> 3,
+            (opcode & 0b00000100) >> 2,
+            (opcode & 0b00000010) >> 1,
+            opcode & 0b00000001,
         );
 
         let pc = match bits {
@@ -47,12 +47,12 @@ impl CPU {
             (0, 0, 1, 0, 1, 0, 1, 0) => self.op_ld_a_hl_inc(),
             (0, 0, 1, 1, 0, 0, 1, 0) => self.op_ld_hl_dec_a(),
             (0, 0, 1, 1, 1, 0, 1, 0) => self.op_ld_a_hl_dec(),
-            (0, 0, _, _, 0, 0, 0, 1) => self.op_ld_rr_nn(byte),
-            (0, 0, _, _, 0, 0, 1, 0) => self.op_ld_rp_a(byte),
-            (0, 0, _, _, 1, 0, 1, 0) => self.op_ld_a_rp(byte),
-            (0, 0, _, _, _, 1, 1, 0) => self.op_ld_r_n(byte),
-            (0, 1, _, _, _, _, _, _) => self.op_ld_r_r(byte),
-            (1, 0, 0, 0, 0, _, _, _) => self.op_add(byte),
+            (0, 0, _, _, 0, 0, 0, 1) => self.op_ld_rr_nn(opcode),
+            (0, 0, _, _, 0, 0, 1, 0) => self.op_ld_rp_a(opcode),
+            (0, 0, _, _, 1, 0, 1, 0) => self.op_ld_a_rp(opcode),
+            (0, 0, _, _, _, 1, 1, 0) => self.op_ld_r_n(opcode),
+            (0, 1, _, _, _, _, _, _) => self.op_ld_r_r(opcode),
+            (1, 0, 0, 0, 0, _, _, _) => self.op_add(opcode),
             (1, 1, 1, 0, 0, 0, 0, 0) => self.op_ldh_n_a(),
             (1, 1, 1, 0, 0, 0, 1, 0) => self.op_ldh_c_a(),
             (1, 1, 1, 0, 1, 0, 1, 0) => self.op_ld_nn_a(),
@@ -66,8 +66,8 @@ impl CPU {
         pc
     }
 
-    fn op_add(&mut self, byte: u8) -> u16 {
-        self.registers.a += self.register_8_get(byte);
+    fn op_add(&mut self, opcode: u8) -> u16 {
+        self.registers.a += self.register_8_get(opcode);
         self.registers.pc + 1
     }
 
@@ -90,8 +90,8 @@ impl CPU {
         self.registers.pc + 3
     }
 
-    fn op_ld_a_rp(&mut self, byte: u8) -> u16 {
-        self.registers.a = self.bus.byte_get(self.register_16_get(byte));
+    fn op_ld_a_rp(&mut self, opcode: u8) -> u16 {
+        self.registers.a = self.bus.byte_get(self.register_16_get(opcode));
         self.registers.pc + 1
     }
 
@@ -119,26 +119,26 @@ impl CPU {
         self.registers.pc + 3
     }
 
-    fn op_ld_r_n(&mut self, byte: u8) -> u16 {
-        self.register_8_set(byte, self.db_get());
+    fn op_ld_r_n(&mut self, opcode: u8) -> u16 {
+        self.register_8_set(opcode, self.db_get());
         self.registers.pc + 2
     }
 
-    fn op_ld_r_r(&mut self, byte: u8) -> u16 {
-        self.register_8_set(byte, self.register_8_get(byte));
+    fn op_ld_r_r(&mut self, opcode: u8) -> u16 {
+        self.register_8_set(opcode, self.register_8_get(opcode));
         self.registers.pc + 1
     }
 
-    fn op_ld_rp_a(&mut self, byte: u8) -> u16 {
-        self.register_16_set_addr(byte, self.registers.a);
+    fn op_ld_rp_a(&mut self, opcode: u8) -> u16 {
+        self.register_16_set_addr(opcode, self.registers.a);
         self.registers.pc + 1
     }
 
-    fn op_ld_rr_nn(&mut self, byte: u8) -> u16 {
+    fn op_ld_rr_nn(&mut self, opcode: u8) -> u16 {
         println!("op_ld_rr_nn");
         let value = self.lb_hb_get();
         println!("{}", value);
-        self.register_16_set(byte, value);
+        self.register_16_set(opcode, value);
         self.registers.pc + 3
     }
 
@@ -171,8 +171,8 @@ impl CPU {
         self.registers.pc + 2
     }
 
-    fn register_16_get(&self, byte: u8) -> u16 {
-        match byte & 0b110000 {
+    fn register_16_get(&self, opcode: u8) -> u16 {
+        match opcode & 0b110000 {
             0b000000 => self.registers.bc_get(),
             0b010000 => self.registers.de_get(),
             0b100000 => self.registers.hl_get(),
@@ -181,8 +181,8 @@ impl CPU {
         }
     }
 
-    fn register_16_set(&mut self, byte: u8, value: u16) {
-        match byte & 0b110000 {
+    fn register_16_set(&mut self, opcode: u8, value: u16) {
+        match opcode & 0b110000 {
             0b000000 => self.registers.bc_set(value),
             0b010000 => self.registers.de_set(value),
             0b100000 => self.registers.hl_set(value),
@@ -191,8 +191,8 @@ impl CPU {
         }
     }
 
-    fn register_16_set_addr(&mut self, byte: u8, value: u8) {
-        match byte & 0b110000 {
+    fn register_16_set_addr(&mut self, opcode: u8, value: u8) {
+        match opcode & 0b110000 {
             0b000000 => self.bus.byte_set(self.registers.bc_get(), value),
             0b010000 => self.bus.byte_set(self.registers.de_get(), value),
             0b100000 => self.bus.byte_set(self.registers.hl_get(), value),
@@ -201,8 +201,8 @@ impl CPU {
         }
     }
 
-    fn register_8_get(&self, byte: u8) -> u8 {
-        match byte & 0b111 {
+    fn register_8_get(&self, opcode: u8) -> u8 {
+        match opcode & 0b111 {
             0b000 => self.registers.b,
             0b001 => self.registers.c,
             0b010 => self.registers.d,
@@ -215,8 +215,8 @@ impl CPU {
         }
     }
 
-    fn register_8_set(&mut self, byte: u8, value: u8) {
-        match byte & 0b111000 {
+    fn register_8_set(&mut self, opcode: u8, value: u8) {
+        match opcode & 0b111000 {
             0b000000 => self.registers.b = value,
             0b001000 => self.registers.c = value,
             0b010000 => self.registers.d = value,
