56a4869dbe44481be7c19e56cbc0fa464d1fe1e2
Masato Bito <m@btmst.jp>
2020-11-16 22:02:49 +0900

    add new CPU struct

--- wasm/src/cpu.rs
+++ wasm/src/cpu.rs
@@ -1,6 +1,12 @@
 use super::mmu::MMU;
 use super::registers::Registers;
 
+macro_rules! op_ld_r_n {
+    ( $self:ident, $register:ident ) => {{
+        $self.registers.$register = $self.fetch_byte();
+    }};
+}
+
 struct CPU {
     mmu: MMU,
     registers: Registers,
@@ -14,243 +20,22 @@ impl CPU {
         }
     }
 
-    pub fn step(&mut self) {
-        let opcode = self.mmu.byte_get(self.registers.pc);
-        self.execute(opcode);
-    }
-
-    fn db_get(&self) -> u8 {
-        self.mmu.byte_get(self.registers.pc + 1)
-    }
-
-    fn lb_hb_get(&self) -> u16 {
-        let lb = self.mmu.byte_get(self.registers.pc + 1);
-        let hb = self.mmu.byte_get(self.registers.pc + 2);
-        (hb as u16) << 8 | lb as u16
-    }
-
-    fn execute(&mut self, opcode: u8) -> u16 {
-        let bits = (
-            (opcode & 0b10000000) >> 7,
-            (opcode & 0b01000000) >> 6,
-            (opcode & 0b00100000) >> 5,
-            (opcode & 0b00010000) >> 4,
-            (opcode & 0b00001000) >> 3,
-            (opcode & 0b00000100) >> 2,
-            (opcode & 0b00000010) >> 1,
-            opcode & 0b00000001,
-        );
-
-        let pc = match bits {
-            (0, 0, 0, 0, 1, 0, 0, 0) => self.op_ld_nn_sp(),
-            (0, 0, 1, 0, 0, 0, 1, 0) => self.op_ld_hl_inc_a(),
-            (0, 0, 1, 0, 1, 0, 1, 0) => self.op_ld_a_hl_inc(),
-            (0, 0, 1, 1, 0, 0, 1, 0) => self.op_ld_hl_dec_a(),
-            (0, 0, 1, 1, 1, 0, 1, 0) => self.op_ld_a_hl_dec(),
-            (0, 0, _, _, 0, 0, 0, 1) => self.op_ld_rr_nn(opcode),
-            (0, 0, _, _, 0, 0, 1, 0) => self.op_ld_rp_a(opcode),
-            (0, 0, _, _, 1, 0, 1, 0) => self.op_ld_a_rp(opcode),
-            (0, 0, _, _, _, 1, 1, 0) => self.op_ld_r_n(opcode),
-            (0, 1, _, _, _, _, _, _) => self.op_ld_r_r(opcode),
-            (1, 0, 0, 0, 0, _, _, _) => self.op_add(opcode),
-            (1, 1, 1, 0, 0, 0, 0, 0) => self.op_ldh_n_a(),
-            (1, 1, 1, 0, 0, 0, 1, 0) => self.op_ldh_c_a(),
-            (1, 1, 1, 0, 1, 0, 1, 0) => self.op_ld_nn_a(),
-            (1, 1, 1, 1, 0, 0, 0, 0) => self.op_ldh_a_n(),
-            (1, 1, 1, 1, 0, 0, 1, 0) => self.op_ldh_a_c(),
-            (1, 1, 1, 1, 1, 0, 0, 1) => self.op_ld_sp_hl(),
-            (1, 1, 1, 1, 1, 0, 1, 0) => self.op_ld_a_nn(),
-            (1, 1, _, _, 0, 0, 0, 1) => self.op_pop_rr(opcode),
-            (1, 1, _, _, 0, 1, 0, 1) => self.op_push_rr(opcode),
-            _ => panic!("not implemented"),
-        };
-
-        pc
-    }
-
-    fn op_add(&mut self, opcode: u8) -> u16 {
-        self.registers.a += self.register_8_get(opcode);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_a_hl_dec(&mut self) -> u16 {
-        let hl = self.registers.hl_get();
-        self.registers.a = self.mmu.byte_get(hl);
-        self.registers.hl_set(hl - 1);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_a_hl_inc(&mut self) -> u16 {
-        let hl = self.registers.hl_get();
-        self.registers.a = self.mmu.byte_get(hl);
-        self.registers.hl_set(hl + 1);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_a_nn(&mut self) -> u16 {
-        self.registers.a = self.mmu.byte_get(self.lb_hb_get());
-        self.registers.pc + 3
-    }
-
-    fn op_ld_a_rp(&mut self, opcode: u8) -> u16 {
-        self.registers.a = self.mmu.byte_get(self.register_16_get(opcode));
-        self.registers.pc + 1
-    }
-
-    fn op_ld_hl_dec_a(&mut self) -> u16 {
-        let hl = self.registers.hl_get();
-        self.mmu.byte_set(hl, self.registers.a);
-        self.registers.hl_set(hl - 1);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_hl_inc_a(&mut self) -> u16 {
-        let hl = self.registers.hl_get();
-        self.mmu.byte_set(hl, self.registers.a);
-        self.registers.hl_set(hl + 1);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_nn_a(&mut self) -> u16 {
-        self.mmu.byte_set(self.lb_hb_get(), self.registers.a);
-        self.registers.pc + 3
-    }
-
-    fn op_ld_nn_sp(&mut self) -> u16 {
-        self.mmu.word_set(self.lb_hb_get(), self.registers.sp);
-        self.registers.pc + 3
-    }
-
-    fn op_ld_r_n(&mut self, opcode: u8) -> u16 {
-        self.register_8_set(opcode, self.db_get());
-        self.registers.pc + 2
-    }
-
-    fn op_ld_r_r(&mut self, opcode: u8) -> u16 {
-        self.register_8_set(opcode, self.register_8_get(opcode));
-        self.registers.pc + 1
-    }
-
-    fn op_ld_rp_a(&mut self, opcode: u8) -> u16 {
-        self.register_16_set_addr(opcode, self.registers.a);
-        self.registers.pc + 1
-    }
-
-    fn op_ld_rr_nn(&mut self, opcode: u8) -> u16 {
-        println!("op_ld_rr_nn");
-        let value = self.lb_hb_get();
-        println!("{}", value);
-        self.register_16_set(opcode, value);
-        self.registers.pc + 3
-    }
-
-    fn op_ld_sp_hl(&mut self) -> u16 {
-        self.registers.sp = self.registers.hl_get();
-        self.registers.pc + 1
-    }
-
-    fn op_ldh_a_c(&mut self) -> u16 {
-        let addr = 0xFF00 | self.registers.c as u16;
-        self.registers.a = self.mmu.byte_get(addr);
-        self.registers.pc + 1
-    }
-
-    fn op_ldh_a_n(&mut self) -> u16 {
-        let addr = 0xFF00 | self.db_get() as u16;
-        self.registers.a = self.mmu.byte_get(addr);
-        self.registers.pc + 2
-    }
-
-    fn op_ldh_c_a(&mut self) -> u16 {
-        let addr = 0xFF00 | self.registers.c as u16;
-        self.mmu.byte_set(addr, self.registers.a);
-        self.registers.pc + 1
-    }
-
-    fn op_ldh_n_a(&mut self) -> u16 {
-        let addr = 0xFF00 | self.db_get() as u16;
-        self.mmu.byte_set(addr, self.registers.a);
-        self.registers.pc + 2
-    }
-
-    fn op_pop_rr(&mut self, opcode: u8) -> u16 {
-        let lb = self.mmu.byte_get(self.registers.sp) as u16;
-        self.registers.sp += 1;
-        let hb = self.mmu.byte_get(self.registers.sp) as u16;
-        self.registers.sp += 1;
-
-        self.register_16_set(opcode, hb << 8 | lb);
-
-        self.registers.pc + 1
-    }
-
-    fn op_push_rr(&mut self, opcode: u8) -> u16 {
-        let value = self.register_16_get(opcode);
-
-        self.registers.sp -= 1;
-        self.mmu.byte_set(self.registers.sp, (value >> 8) as u8);
-        self.registers.sp -= 1;
-        self.mmu.byte_set(self.registers.sp, (value & 0xFF) as u8);
-
-        self.registers.pc + 1
-    }
-
-    fn register_16_get(&self, opcode: u8) -> u16 {
-        match opcode & 0b110000 {
-            0b000000 => self.registers.bc_get(),
-            0b010000 => self.registers.de_get(),
-            0b100000 => self.registers.hl_get(),
-            0b110000 => self.registers.sp as u16,
-            _ => panic!("never reach"),
+    fn execute(&mut self) {
+        match self.fetch_byte() {
+            0x06 => op_ld_r_n!(self, b),
+            0x0E => op_ld_r_n!(self, c),
+            0x16 => op_ld_r_n!(self, d),
+            0x1E => op_ld_r_n!(self, e),
+            0x26 => op_ld_r_n!(self, h),
+            0x2E => op_ld_r_n!(self, l),
+            _ => panic!("not implemented instruction"),
         }
     }
 
-    fn register_16_set(&mut self, opcode: u8, value: u16) {
-        match opcode & 0b110000 {
-            0b000000 => self.registers.bc_set(value),
-            0b010000 => self.registers.de_set(value),
-            0b100000 => self.registers.hl_set(value),
-            0b110000 => self.registers.sp = value,
-            _ => panic!("never reach"),
-        }
-    }
-
-    fn register_16_set_addr(&mut self, opcode: u8, value: u8) {
-        match opcode & 0b110000 {
-            0b000000 => self.mmu.byte_set(self.registers.bc_get(), value),
-            0b010000 => self.mmu.byte_set(self.registers.de_get(), value),
-            0b100000 => self.mmu.byte_set(self.registers.hl_get(), value),
-            0b110000 => self.mmu.byte_set(self.registers.sp, value),
-            _ => panic!("never reach"),
-        }
-    }
-
-    fn register_8_get(&self, opcode: u8) -> u8 {
-        match opcode & 0b111 {
-            0b000 => self.registers.b,
-            0b001 => self.registers.c,
-            0b010 => self.registers.d,
-            0b011 => self.registers.e,
-            0b100 => self.registers.h,
-            0b101 => self.registers.l,
-            0b110 => self.mmu.byte_get(self.registers.hl_get()),
-            0b111 => self.registers.a,
-            _ => panic!("never reach"),
-        }
-    }
-
-    fn register_8_set(&mut self, opcode: u8, value: u8) {
-        match opcode & 0b111000 {
-            0b000000 => self.registers.b = value,
-            0b001000 => self.registers.c = value,
-            0b010000 => self.registers.d = value,
-            0b011000 => self.registers.e = value,
-            0b100000 => self.registers.h = value,
-            0b101000 => self.registers.l = value,
-            0b110000 => self.mmu.byte_set(self.registers.hl_get(), value),
-            0b111000 => self.registers.a = value,
-            _ => panic!("invalid destination register"),
-        }
+    fn fetch_byte(&mut self) -> u8 {
+        let byte = self.mmu.byte_get(self.registers.pc);
+        self.registers.pc += 1;
+        byte
     }
 }
 
