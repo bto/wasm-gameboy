c797e37bacfd8ad28a5ccad4483a1133cd3a1b70
Masato Bito <m@btmst.jp>
2020-11-17 17:28:54 +0900

    add LD r, r

--- wasm/src/cpu.rs
+++ wasm/src/cpu.rs
@@ -2,8 +2,14 @@ use super::mmu::MMU;
 use super::registers::Registers;
 
 macro_rules! op_ld_r_n {
-    ( $self:ident, $register:ident ) => {{
-        $self.registers.$register = $self.fetch_byte();
+    ( $self:ident, $dest:ident ) => {{
+        $self.registers.$dest = $self.fetch_byte();
+    }};
+}
+
+macro_rules! op_ld_r_r {
+    ( $self:ident, $dest:ident, $src:ident ) => {{
+        $self.registers.$dest = $self.registers.$src;
     }};
 }
 
@@ -29,6 +35,55 @@ impl CPU {
             0b00_100_110 => op_ld_r_n!(self, h),
             0b00_101_110 => op_ld_r_n!(self, l),
             0b00_111_110 => op_ld_r_n!(self, a),
+            0b01_000_000 => op_ld_r_r!(self, b, b),
+            0b01_000_001 => op_ld_r_r!(self, b, c),
+            0b01_000_010 => op_ld_r_r!(self, b, d),
+            0b01_000_011 => op_ld_r_r!(self, b, e),
+            0b01_000_100 => op_ld_r_r!(self, b, h),
+            0b01_000_101 => op_ld_r_r!(self, b, l),
+            0b01_000_111 => op_ld_r_r!(self, b, a),
+            0b01_001_000 => op_ld_r_r!(self, c, b),
+            0b01_001_001 => op_ld_r_r!(self, c, c),
+            0b01_001_010 => op_ld_r_r!(self, c, d),
+            0b01_001_011 => op_ld_r_r!(self, c, e),
+            0b01_001_100 => op_ld_r_r!(self, c, h),
+            0b01_001_101 => op_ld_r_r!(self, c, l),
+            0b01_001_111 => op_ld_r_r!(self, c, a),
+            0b01_010_000 => op_ld_r_r!(self, d, b),
+            0b01_010_001 => op_ld_r_r!(self, d, c),
+            0b01_010_010 => op_ld_r_r!(self, d, d),
+            0b01_010_011 => op_ld_r_r!(self, d, e),
+            0b01_010_100 => op_ld_r_r!(self, d, h),
+            0b01_010_101 => op_ld_r_r!(self, d, l),
+            0b01_010_111 => op_ld_r_r!(self, d, a),
+            0b01_011_000 => op_ld_r_r!(self, e, b),
+            0b01_011_001 => op_ld_r_r!(self, e, c),
+            0b01_011_010 => op_ld_r_r!(self, e, d),
+            0b01_011_011 => op_ld_r_r!(self, e, e),
+            0b01_011_100 => op_ld_r_r!(self, e, h),
+            0b01_011_101 => op_ld_r_r!(self, e, l),
+            0b01_011_111 => op_ld_r_r!(self, e, a),
+            0b01_100_000 => op_ld_r_r!(self, h, b),
+            0b01_100_001 => op_ld_r_r!(self, h, c),
+            0b01_100_010 => op_ld_r_r!(self, h, d),
+            0b01_100_011 => op_ld_r_r!(self, h, e),
+            0b01_100_100 => op_ld_r_r!(self, h, h),
+            0b01_100_101 => op_ld_r_r!(self, h, l),
+            0b01_100_111 => op_ld_r_r!(self, h, a),
+            0b01_101_000 => op_ld_r_r!(self, l, b),
+            0b01_101_001 => op_ld_r_r!(self, l, c),
+            0b01_101_010 => op_ld_r_r!(self, l, d),
+            0b01_101_011 => op_ld_r_r!(self, l, e),
+            0b01_101_100 => op_ld_r_r!(self, l, h),
+            0b01_101_101 => op_ld_r_r!(self, l, l),
+            0b01_101_111 => op_ld_r_r!(self, l, a),
+            0b01_111_000 => op_ld_r_r!(self, a, b),
+            0b01_111_001 => op_ld_r_r!(self, a, c),
+            0b01_111_010 => op_ld_r_r!(self, a, d),
+            0b01_111_011 => op_ld_r_r!(self, a, e),
+            0b01_111_100 => op_ld_r_r!(self, a, h),
+            0b01_111_101 => op_ld_r_r!(self, a, l),
+            0b01_111_111 => op_ld_r_r!(self, a, a),
             _ => panic!("not implemented instruction"),
         }
     }
--- wasm/src/cpu_test.rs
+++ wasm/src/cpu_test.rs
@@ -5,6 +5,10 @@ macro_rules! set_inst {
         $cpu.mmu.byte_set($pc + 0, $v1);
         $cpu.mmu.byte_set($pc + 1, $v2);
     };
+
+    ( $cpu:ident, $pc:ident, $v1:expr ) => {
+        $cpu.mmu.byte_set($pc + 0, $v1);
+    };
 }
 
 #[test]
@@ -22,18 +26,61 @@ fn test_op_ld_r_n() {
     for i in [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b111].iter() {
         let pc = cpu.registers.pc;
         let opcode = opcode_base | (i << 3);
-        set_inst!(cpu, pc, opcode, *i);
+        let v = *i;
+        set_inst!(cpu, pc, opcode, v);
+
         cpu.execute();
+
         assert_eq!(cpu.registers.pc, pc + 2);
         match i {
-            0b000 => assert_eq!(cpu.registers.b, *i),
-            0b001 => assert_eq!(cpu.registers.c, *i),
-            0b010 => assert_eq!(cpu.registers.d, *i),
-            0b011 => assert_eq!(cpu.registers.e, *i),
-            0b100 => assert_eq!(cpu.registers.h, *i),
-            0b101 => assert_eq!(cpu.registers.l, *i),
-            0b111 => assert_eq!(cpu.registers.a, *i),
+            0b000 => assert_eq!(cpu.registers.b, v),
+            0b001 => assert_eq!(cpu.registers.c, v),
+            0b010 => assert_eq!(cpu.registers.d, v),
+            0b011 => assert_eq!(cpu.registers.e, v),
+            0b100 => assert_eq!(cpu.registers.h, v),
+            0b101 => assert_eq!(cpu.registers.l, v),
+            0b111 => assert_eq!(cpu.registers.a, v),
             _ => panic!("never reach"),
+        }
+    }
+}
+
+#[test]
+fn test_op_ld_r_r() {
+    let mut cpu = CPU::new();
+
+    let opcode_base = 0b01_000_000;
+    for i in [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b111].iter() {
+        for j in [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b111].iter() {
+            let pc = cpu.registers.pc;
+            let opcode = opcode_base | (i << 3) | j;
+            set_inst!(cpu, pc, opcode);
+
+            let v = i << 4 | j;
+            match j {
+                0b000 => cpu.registers.b = v,
+                0b001 => cpu.registers.c = v,
+                0b010 => cpu.registers.d = v,
+                0b011 => cpu.registers.e = v,
+                0b100 => cpu.registers.h = v,
+                0b101 => cpu.registers.l = v,
+                0b111 => cpu.registers.a = v,
+                _ => panic!("never reach"),
+            }
+
+            cpu.execute();
+
+            assert_eq!(cpu.registers.pc, pc + 1);
+            match i {
+                0b000 => assert_eq!(cpu.registers.b, v),
+                0b001 => assert_eq!(cpu.registers.c, v),
+                0b010 => assert_eq!(cpu.registers.d, v),
+                0b011 => assert_eq!(cpu.registers.e, v),
+                0b100 => assert_eq!(cpu.registers.h, v),
+                0b101 => assert_eq!(cpu.registers.l, v),
+                0b111 => assert_eq!(cpu.registers.a, v),
+                _ => panic!("never reach"),
+            }
         }
     }
 }
