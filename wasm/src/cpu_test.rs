cd444de9e376bd5201436423f6191aac33a3ade2
Masato Bito <m@btmst.jp>
2020-11-29 11:37:41 +0900

    fix by lint

--- wasm/src/cpu_test.rs
+++ wasm/src/cpu_test.rs
@@ -1,21 +1,21 @@
 #[allow(unused_imports)]
 use super::*;
 
+#[allow(unused_macros)]
 macro_rules! set_inst {
-    ( $cpu:ident, $pc:ident, $v1:expr ) => {
-        $cpu.mmu.byte_set($pc + 0, $v1);
-    };
+    ( $cpu:ident, $pc:ident, $v1:expr ) => {{
+        $cpu.mmu.byte_set($pc + 0, $v1)
+    }};
 
-    ( $cpu:ident, $pc:ident, $v1:expr, $v2:expr ) => {
-        $cpu.mmu.byte_set($pc + 0, $v1);
-        $cpu.mmu.byte_set($pc + 1, $v2);
-    };
+    ( $cpu:ident, $pc:ident, $v1:expr, $v2:expr ) => {{
+        set_inst!($cpu, $pc, $v1);
+        $cpu.mmu.byte_set($pc + 1, $v2)
+    }};
 
-    ( $cpu:ident, $pc:ident, $v1:expr, $v2:expr, $v3:expr ) => {
-        $cpu.mmu.byte_set($pc + 0, $v1);
-        $cpu.mmu.byte_set($pc + 1, $v2);
-        $cpu.mmu.byte_set($pc + 2, $v3);
-    };
+    ( $cpu:ident, $pc:ident, $v1:expr, $v2:expr, $v3:expr ) => {{
+        set_inst!($cpu, $pc, $v1, $v2);
+        $cpu.mmu.byte_set($pc + 2, $v3)
+    }};
 }
 
 #[test]
