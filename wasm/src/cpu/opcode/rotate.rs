f55a90d4823ae285748fe292121b326622e1b913
Masato Bito <m@btmst.jp>
2020-12-02 23:48:22 +0900

    improve macro

--- wasm/src/cpu/opcode/rotate.rs
+++ wasm/src/cpu/opcode/rotate.rs
@@ -1,13 +1,19 @@
+macro_rules! set_rotate_flags {
+    ( $self:ident, $result:expr, $carry:expr ) => {{
+        $self.registers.carry = $carry;
+        $self.registers.half_carry = false;
+        $self.registers.subtraction = false;
+        $self.registers.zero = $result == 0;
+    }};
+}
+
 macro_rules! op_rl_r {
     ( $self:ident, $dest:ident ) => {{
         let x = $self.registers.$dest;
         let c = x & 0x80 == 0x80;
         let r = x << 1 | $self.registers.carry as u8;
         $self.registers.$dest = r;
-        $self.registers.carry = c;
-        $self.registers.half_carry = false;
-        $self.registers.subtraction = false;
-        $self.registers.zero = r == 0;
+        set_rotate_flags!($self, r, c);
     }};
 }
 
@@ -17,10 +23,7 @@ macro_rules! op_rlc_r {
         let c = x & 0x80 == 0x80;
         let r = x << 1 | c as u8;
         $self.registers.$dest = r;
-        $self.registers.carry = c;
-        $self.registers.half_carry = false;
-        $self.registers.subtraction = false;
-        $self.registers.zero = r == 0;
+        set_rotate_flags!($self, r, c);
     }};
 }
 
@@ -30,10 +33,7 @@ macro_rules! op_rr_r {
         let c = x & 0x01 == 0x01;
         let r = x >> 1 | ($self.registers.carry as u8) << 7;
         $self.registers.$dest = r;
-        $self.registers.carry = c;
-        $self.registers.half_carry = false;
-        $self.registers.subtraction = false;
-        $self.registers.zero = r == 0;
+        set_rotate_flags!($self, r, c);
     }};
 }
 
@@ -43,9 +43,6 @@ macro_rules! op_rrc_r {
         let c = x & 0x01 == 0x01;
         let r = x >> 1 | (c as u8) << 7;
         $self.registers.$dest = r;
-        $self.registers.carry = c;
-        $self.registers.half_carry = false;
-        $self.registers.subtraction = false;
-        $self.registers.zero = r == 0;
+        set_rotate_flags!($self, r, c);
     }};
 }
