40edb746dd34b7b6ab365ce0698782128a0199ee
Masato Bito <m@btmst.jp>
2020-12-02 13:24:00 +0900

    add half carry test

--- wasm/src/cpu/opcode/dec_tests.rs
+++ wasm/src/cpu/opcode/dec_tests.rs
@@ -11,7 +11,41 @@ fn op_dec_r() {
     for i in [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b111].iter() {
         let opcode = opcode_base | (i << 3);
 
-        // not overflow
+        // half carry
+        let pc = cpu.registers.pc;
+        set_inst!(cpu, pc, opcode);
+        match i {
+            0b000 => cpu.registers.b = 0b1_0000,
+            0b001 => cpu.registers.c = 0b1_0000,
+            0b010 => cpu.registers.d = 0b1_0000,
+            0b011 => cpu.registers.e = 0b1_0000,
+            0b100 => cpu.registers.h = 0b1_0000,
+            0b101 => cpu.registers.l = 0b1_0000,
+            0b111 => cpu.registers.a = 0b1_0000,
+            _ => panic!("never reach"),
+        }
+        cpu.registers.carry = true;
+        cpu.registers.half_carry = false;
+        cpu.registers.subtraction = false;
+        cpu.registers.zero = true;
+        cpu.execute();
+        assert_eq!(cpu.registers.pc, pc + 1);
+        assert_eq!(cpu.registers.carry, true);
+        assert_eq!(cpu.registers.half_carry, true);
+        assert_eq!(cpu.registers.subtraction, true);
+        assert_eq!(cpu.registers.zero, false);
+        match i {
+            0b000 => assert_eq!(cpu.registers.b, 0b0_1111),
+            0b001 => assert_eq!(cpu.registers.c, 0b0_1111),
+            0b010 => assert_eq!(cpu.registers.d, 0b0_1111),
+            0b011 => assert_eq!(cpu.registers.e, 0b0_1111),
+            0b100 => assert_eq!(cpu.registers.h, 0b0_1111),
+            0b101 => assert_eq!(cpu.registers.l, 0b0_1111),
+            0b111 => assert_eq!(cpu.registers.a, 0b0_1111),
+            _ => panic!("never reach"),
+        }
+
+        // zero
         let pc = cpu.registers.pc;
         set_inst!(cpu, pc, opcode);
         match i {
@@ -152,7 +186,25 @@ fn op_dec_rrn() {
     let mut cpu = CPU::new();
     let opcode = 0b00_110_101;
 
-    // not overflow
+    // half carry
+    let pc = cpu.registers.pc;
+    set_inst!(cpu, pc, opcode);
+    cpu.registers.h = 1;
+    cpu.registers.l = 2;
+    cpu.mmu.byte_set(0x102, 0b1_0000);
+    cpu.registers.carry = true;
+    cpu.registers.half_carry = false;
+    cpu.registers.subtraction = false;
+    cpu.registers.zero = true;
+    cpu.execute();
+    assert_eq!(cpu.registers.pc, pc + 1);
+    assert_eq!(cpu.registers.carry, true);
+    assert_eq!(cpu.registers.half_carry, true);
+    assert_eq!(cpu.registers.subtraction, true);
+    assert_eq!(cpu.registers.zero, false);
+    assert_eq!(cpu.mmu.byte_get(0x102), 0b0_1111);
+
+    // zero
     let pc = cpu.registers.pc;
     set_inst!(cpu, pc, opcode);
     cpu.registers.h = 1;
