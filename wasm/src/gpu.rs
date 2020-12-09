4c5322047d139e52999cac43903222d2eb7ed5bb
Masato Bito <m@btmst.jp>
2020-12-10 08:39:43 +0900

    use gpu

--- wasm/src/gpu.rs
+++ wasm/src/gpu.rs
@@ -2,17 +2,24 @@ const VRAM_BEGIN: usize = 0x8000;
 const VRAM_END: usize = 0x9FFF;
 const VRAM_SIZE: usize = VRAM_END - VRAM_BEGIN + 1;
 
-struct GPU {
+#[derive(Debug, PartialEq)]
+pub struct GPU {
     vram: [u8; VRAM_SIZE],
 }
 
 impl GPU {
-    fn byte_get(&self, addr: u16) -> u8 {
+    pub fn new() -> Self {
+        Self {
+            vram: [0u8; VRAM_SIZE],
+        }
+    }
+
+    pub fn byte_get(&self, addr: u16) -> u8 {
         let addr = addr as usize - VRAM_BEGIN;
         self.vram[addr]
     }
 
-    fn byte_write(&mut self, addr: u16, value: u8) {
+    pub fn byte_set(&mut self, addr: u16, value: u8) {
         let addr = addr as usize - VRAM_BEGIN;
         self.vram[addr] = value;
     }
--- wasm/src/mmu.rs
+++ wasm/src/mmu.rs
@@ -1,32 +1,42 @@
+use super::gpu::GPU;
+
 #[derive(Debug, PartialEq)]
 pub struct MMU {
+    gpu: GPU,
     memory: [u8; 0xFFFF],
 }
 
 impl MMU {
     pub fn new() -> Self {
         Self {
+            gpu: GPU::new(),
             memory: [0u8; 0xFFFF],
         }
     }
 
     pub fn byte_get(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
+        match addr {
+            0x8000..=0x9FFF => self.gpu.byte_get(addr),
+            _ => self.memory[addr as usize],
+        }
     }
 
     pub fn byte_set(&mut self, addr: u16, value: u8) {
-        self.memory[addr as usize] = value;
+        match addr {
+            0x8000..=0x9FFF => self.gpu.byte_set(addr, value),
+            _ => self.memory[addr as usize] = value,
+        }
     }
 
     pub fn word_get(&self, addr: u16) -> u16 {
-        let lb = self.memory[addr as usize];
-        let hb = self.memory[(addr + 1) as usize];
+        let lb = self.byte_get(addr);
+        let hb = self.byte_get(addr + 1);
         (hb as u16) << 8 | lb as u16
     }
 
     pub fn word_set(&mut self, addr: u16, value: u16) {
-        self.memory[addr as usize] = value as u8;
-        self.memory[(addr + 1) as usize] = (value >> 8) as u8;
+        self.byte_set(addr, value as u8);
+        self.byte_set(addr + 1, (value >> 8) as u8);
     }
 }
 
